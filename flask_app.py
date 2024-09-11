15936ec880f6eadf15a18667fcec177469f349fe
Azamku <aziz.amkouy@gmail.com>
2024-09-11 19:12:33 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -164,7 +164,7 @@ except Exception as e:
 #@app.route("/", methods=["GET,POST"])
 @app.route("/")
 def home():
-    return jsonify({"message": "Bienvenue dans l'API de prédiction de tags. Consultez /docs pour plus d'informations. Test du 11Sept 18H45"})
+    return jsonify({"message": "Bienvenue dans l'API de prédiction de tags. Consultez /docs pour plus d'informations. Test du 11Sept 19H12"})
 
 
 #@app.route("/predict", methods=["POST"])
